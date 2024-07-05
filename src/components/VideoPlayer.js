25fd236150984f9a24773bda742cba6d224ad7dd
Bappy38 <iqbal.bappy.13@gmail.com>
2024-07-05 11:40:47 +0600

    VideoPlayer's ButtonContainer Issue Fix

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -9,7 +9,7 @@ const VideoPlayer = (props) => {
 
     return (
         <div className="">
-            <div className="mt-2 rounded-lg">
+            <div className="rounded-lg">
                 <iframe
                     className="rounded-xl"
                     width="660"
@@ -25,30 +25,32 @@ const VideoPlayer = (props) => {
             <div className="w-[660px] mt-2">
                 <div className="font-semibold text-xl">{videoTitle}</div>
             </div>
-            <div className="flex mt-3 h-10">
-                <img className="h-11 w-11 cursor-pointer rounded-full" alt="Logo" src={channelLogo} />
+            <div className="flex mt-3 h-10 mx-auto">
+                <div className="flex">
+                    <img className="h-11 w-11 cursor-pointer rounded-full" alt="Logo" src={channelLogo} />
 
-                <div className="ml-3">
-                    <h1 className="font-semibold cursor-pointer">{channelTitle}</h1>
-                    <div className="text-gray-600 font-normal text-sm">
-                        {toFormattedNumber(subscriberCount)} subscribers
+                    <div className="ml-3">
+                        <h1 className="font-semibold cursor-pointer">{channelTitle}</h1>
+                        <div className="text-gray-600 font-normal text-sm">
+                            {toFormattedNumber(subscriberCount)} subscribers
+                        </div>
                     </div>
+
+                    <button
+                        className="
+                            ml-5
+                            bg-gray-950
+                            text-white 
+                            font-medium 
+                            rounded-full
+                            px-4
+                            h-full"
+                    >
+                        Subscribe
+                    </button>
                 </div>
 
-                <button
-                    className="
-                        ml-5
-                        bg-gray-950
-                        text-white 
-                        font-medium 
-                        rounded-full
-                        px-4
-                        h-full"
-                >
-                    Subscribe
-                </button>
-
-                <div className="flex ml-28">
+                <div className="flex ml-auto">
                     <div className="bg-zinc-100 ml-5 rounded-full">
                         <button className="cursor-pointer px-3 h-full border-r border-gray-200 rounded-l-full hover:bg-gray-300">
                             <FontAwesomeIcon icon={faThumbsUp} />
